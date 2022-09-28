e52614147a4e7daef2e1fbd788a0b97378d660a1
fdisano <feliciadisano@gmail.com>
2022-09-27 23:15:02 -0400

    redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,8 +25,8 @@ export class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+      this.toggleModal();
+      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -106,7 +106,7 @@ function RenderDish({dish}){
     }
 }
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, dishId}){
     if (comments != null){
         return (
           <div className="col-12 col-md-5 m-1">
@@ -121,7 +121,7 @@ function RenderComments({comments}){
                       );
                   })}
               </ul>
-              <CommentForm />
+              <CommentForm dishId={dishId} addComment={addComment}/>
           </div>
         );
     }
@@ -148,7 +148,7 @@ const DishDetail = (props) =>  {
               </div>
               <div className="row">
                   <RenderDish dish={props.dish} />
-                  <RenderComments comments={props.comments} />
+                  <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
               </div>
           </div>
         );
